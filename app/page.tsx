07a7f3097acae05461e474c7d3c67c74e165b29c
James Wu <88938117+thejwuscript@users.noreply.github.com>
2024-02-16 01:58:23 +0900

    Use socket.io client to connect to server

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,15 @@
+'use client'
+
+import { useEffect } from 'react';
+import { io } from 'socket.io-client';
+
+
 export default function Home() {
+  useEffect(() => {
+    const socket = io('http://localhost:3001');
+
+  }, [])
   return (
-    <div></div>
+    <div>Home page</div>
   );
 }
