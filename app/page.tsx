d1ed9e22ed2cc0b022473f280b7ac6711e82176f
James Wu <88938117+thejwuscript@users.noreply.github.com>
2024-02-16 22:48:34 +0900

    Display user connection status

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,28 @@
 import { useEffect } from 'react';
 import { io } from 'socket.io-client';
 
-
 export default function Home() {
   useEffect(() => {
     const socket = io('http://localhost:3001');
 
+    // socket.on('connect', () => {
+    //   const socketId = socket.id;
+    //   console.log("Socket ID:", socketId);
+    // });
+
+    socket.on("user connected", user => {
+      console.log(`User ${user.name} has connected.`)
+    })
+
+    socket.on("user disconnected", user => {
+      console.log(`User ${user.name} has disconnected.`)
+    })
+
+    return () => {
+      socket.disconnect()
+    }
   }, [])
+
   return (
     <div>Home page</div>
   );
