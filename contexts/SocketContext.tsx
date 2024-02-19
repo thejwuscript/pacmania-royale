a4a6f53eb1b03b0da9809c8c06cd01dbc806f7ca
James Wu <88938117+thejwuscript@users.noreply.github.com>
2024-02-19 17:19:42 +0900

    Fix children type

--- contexts/SocketContext.tsx
+++ contexts/SocketContext.tsx
@@ -1,10 +1,10 @@
-import { createContext, PropsWithChildren } from "react";
+import { createContext } from "react";
 import { io, Socket } from "socket.io-client";
 
 export const SocketContext = createContext<Socket | null>(null);
 
-export function SocketProvider({ children }: PropsWithChildren) {
-  const socket = io();
+export function SocketProvider({ children }: { children: React.ReactNode }) {
+  const socket = io(process.env.NEXT_PUBLIC_SOCKETIO_URL);
 
   return (
     <SocketContext.Provider value={socket}>
