529d74357ec6ca448268db9071a944afec150e91
James Wu <88938117+thejwuscript@users.noreply.github.com>
2024-02-29 01:58:46 +0900

    Player can join a game room

--- app/gameroom/[id]/page.tsx
+++ app/gameroom/[id]/page.tsx
@@ -1,7 +1,26 @@
+'use client';
+
+import { useState, useContext } from 'react';
+import type { User } from '@/types/common';
+import { SocketContext } from '@/components/SocketProvider';
+
+type Player = User;
+
 export default function Gameroom({ params }: { params: { id: string } }) {
+  const socket = useContext(SocketContext);
+  const [players, setPlayers] = useState<Player[]>([]);
+
   return (
     <div>
       Welcome to game room {params.id}
+      <h2>Players in this room are:</h2>
+      <ul>
+        {players.map(player => (
+          <li>
+            {player.name}
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
