c923772f45856b08692e910a752005b3e78255b8
James Wu <88938117+thejwuscript@users.noreply.github.com>
2024-03-14 01:15:21 +0900

    Make game over scene

--- components/Game/scenes/GameOver.ts
+++ components/Game/scenes/GameOver.ts
@@ -1,7 +1,31 @@
 import { Scene } from "phaser";
 
+type TextStyle = Phaser.Types.GameObjects.Text.TextStyle;
+
 export class GameOver extends Scene {
+  name: string;
+
   constructor() {
     super("GameOver");
+    this.name = "";
+  }
+
+  init(data: { name: string }) {
+    this.name = data.name;
+  }
+
+  create() {
+    const centerX = this.cameras.main.centerX;
+    const centerY = this.cameras.main.centerY;
+
+    const textStyle: TextStyle = {
+      fontFamily: "Times New Roman",
+      fontSize: "28px",
+    };
+
+    this.add.text(centerX, centerY - 50, "Game Over", textStyle).setOrigin(0.5);
+
+    const message = `${this.name} wins the game!`;
+    this.add.text(centerX, centerY + 50, message, textStyle).setOrigin(0.5);
   }
 }
