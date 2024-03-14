ef239fd7caa40249f8ff90876bd29b22eac0013f
James Wu <88938117+thejwuscript@users.noreply.github.com>
2024-03-14 12:41:45 +0900

    Add some padding to link

--- components/LobbyGameroomListItem.tsx
+++ components/LobbyGameroomListItem.tsx
@@ -8,14 +8,14 @@ interface LobbyGameroomListItemProps {
 
 export default function LobbyGameroomListItem({ id, maxPlayerCount, currentPlayerCount }: LobbyGameroomListItemProps) {
   return (
-    <li className="max-w-[400px] flex justify-around">
+    <li className="max-w-[400px] flex justify-around items-center">
       <div>
         Game Room #{id}, Players {currentPlayerCount}/{maxPlayerCount}
       </div>
       {currentPlayerCount >= maxPlayerCount ? (
         <div className="italic">Room Full</div>
       ) : (
-        <Link href={`/gameroom/${id}`} className="hover:underline underline-offset-2">
+        <Link href={`/gameroom/${id}`} className="hover:underline underline-offset-2 px-2 py-1">
           Join Room
         </Link>
       )}
