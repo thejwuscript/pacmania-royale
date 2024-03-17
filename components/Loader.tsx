6e6548c0d7eef5bac348835c972476a5a506f829
James Wu <88938117+thejwuscript@users.noreply.github.com>
2024-03-18 01:56:49 +0900

    Show wait message after few seconds

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,9 +1,23 @@
+import { useState, useEffect } from "react";
 import { RotatingLines } from "react-loader-spinner";
 
 export default function Loader() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    setTimeout(() => setMessage("Please wait..."), 4000);
+  }, []);
+
   return (
     <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-      <RotatingLines visible={true} strokeColor="grey" strokeWidth="5" animationDuration="0.75" ariaLabel="rotating-lines-loading" />
+      <RotatingLines
+        visible={true}
+        strokeColor="grey"
+        strokeWidth="5"
+        animationDuration="0.75"
+        ariaLabel="rotating-lines-loading"
+      />
+      {message && <p className="mt-2">{message}</p>}
     </div>
-  )
+  );
 }
