236b7ad39e2da6768361f05dfdad481a5590ad32
James Wu <88938117+thejwuscript@users.noreply.github.com>
2024-03-18 17:05:04 +0900

    Add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import ThemeProvider from "@/components/ThemeProvider";
 import SocketProvider from "@/components/SocketProvider";
 import NavigationEvents from "@/components/NavigationEvents";
+import { Analytics } from '@vercel/analytics/react';
 
 const pressStart2P = localFont({ src: "../fonts/PressStart2P-Regular.ttf", variable: "--font-press-start-2p" });
 const inter = localFont({ src: "../fonts/Inter-VariableFont.ttf" });
@@ -25,6 +26,7 @@ export default function RootLayout({
         <SocketProvider>
           <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
             {children}
+            <Analytics />
           </ThemeProvider>
         </SocketProvider>
       </body>
